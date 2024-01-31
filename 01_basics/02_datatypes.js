794923444a4fc689be57b9b881ea1e4643d13685
Kaifshk1707 <154221535+Kaifshk1707@users.noreply.github.com>
2024-01-31 11:23:48 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+// "use strict" // treat all the code are newer version of js
+
+// alert("Hello World")
+
+console.log(3   
+    + 4);  // Code hamesha readable hona chahiye
+
+    console.log("Hello World");
+
+//data types in js
+// 1. String
+// 2. Numbers
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Object
+// 7. Symbol
+
+console.log(typeof("Hello Wolrd"));
+console.log(typeof(123));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof({}));
