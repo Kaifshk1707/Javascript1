e002266fe58183b711f5e40bc9286cc7280fd3a2
Kaifshk1707 <154221535+Kaifshk1707@users.noreply.github.com>
2024-02-14 16:34:27 +0530

    test

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+for (let i = 0; i <= 1000000000; i++) {
+    if (i== 1) {
+        console.log(`The actual number is ${i} and the loop is break`);
+        
+    }
+
+    console.log(`The number is ${i}`);
+}
